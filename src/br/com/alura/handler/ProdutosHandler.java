37a2dd019f73282328dc42467dfb51048372ff9d
William Mian <william_mian@yahoo.com.br>
2020-08-31 07:48:24 -0300

    aula

---
+++ src/br/com/alura/handler/ProdutosHandler.java
@@ -0,0 +1,47 @@
+package br.com.alura.handler;
+
+import br.com.alura.model.Produto;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutosHandler extends DefaultHandler {
+
+    private List<Produto> produtos = new ArrayList<>();
+    private StringBuilder conteudo;
+    private Produto produto;
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if(qName.equals("produto")) {
+            produto = new Produto();
+        }
+
+        conteudo = new StringBuilder();
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        conteudo.append(new String(ch, start, length));
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if(qName.equals("produto")) {
+            produtos.add(produto);
+        }else if(qName.equals("nome")) {
+            produto.setNome(conteudo.toString());
+        }else if(qName.equals("preco")) {
+            double preco = Double.parseDouble(conteudo.toString());
+            produto.setPreco(preco);
+        }
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+}
